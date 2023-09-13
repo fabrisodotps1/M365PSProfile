a7e6cfd17ab06284bc03b07b9969a2237d691330
Andres Bohren <a.bohren@icewolf.ch>
2023-09-13 17:21:01 +0200

    Initial PS Profile

---
+++ profile.ps1
@@ -0,0 +1,11 @@
+###############################################################################
+# M365 PS Profle 
+###############################################################################
+#Zielvorstellung
+#•	Im Profil nur noch das Modul laden
+#•	Parameter für die zu installierenden Module
+#•	Soll PowerShell 5 / 7 Unterstützen (Allenfalls https://github.com/JustinGrote/ModuleFast für PowerShell 7)
+#•	https://blog.icewolf.ch/archive/2023/08/12/PowerShellGet-will-be-renamed-to-Microsoft-PowerShell-PSResourceGet/
+#•	Soll keine Admin Rechte brauchten > Sondern im CurrentUser Kontext funktionieren
+#•	Aktuell dauert AZ Installation ewig. Evtl müssen wir da die installierten Module mit den Dependencys prüfen. Allenfalls kann man die Installation auch paralellisieren
+###############################################################################
