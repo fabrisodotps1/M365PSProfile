f6353db29da2cf500f7bbc48ffc6fb7a06c96aea
Andres Bohren <a.bohren@icewolf.ch>
2023-11-20 18:58:28 +0100

    Added Write-Host to Uninstall Function

--- M365PSProfile.psm1
+++ M365PSProfile.psm1
@@ -95,6 +95,7 @@ Function Uninstall-M365Module {
 				Write-Host "WARNING: PS must be running <As Administrator> to uninstall the Module" -ForegroundColor Red
 			} else {
 				# Uninstall all versions of the module
+				Write-Host "Uninstall Module: $Module $($InstalledModules.Version.ToString())" -ForegroundColor Yellow
 				Uninstall-PSResource -Name $Module -Scope $Scope -SkipDependencyCheck -WarningAction SilentlyContinue
 			}
 		}
